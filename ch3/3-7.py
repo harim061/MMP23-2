c4e715bef9b917ff3c9fe37716ff33030d11f695
harim061 <doris5093@duksung.ac.kr>
2023-10-06 00:52:04 +0900

    9/26장

--- ch3/3-7.py
+++ ch3/3-7.py
@@ -7,16 +7,22 @@ gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.putText(gray,'soccer',(10,20),cv.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),2)
 cv.imshow('Original',gray)
 
+# GaussianBlur(이미지,(필터크기,필터크기),시그마값 = 종의 높이)
 smooth=np.hstack((cv.GaussianBlur(gray,(5,5),0.0),cv.GaussianBlur(gray,(9,9),0.0),cv.GaussianBlur(gray,(15,15),0.0)))
 cv.imshow('Smooth',smooth)
 
+# 3x3
 femboss=np.array([[-1.0, 0.0, 0.0],
                   [ 0.0, 0.0, 0.0],
                   [ 0.0, 0.0, 1.0]])
 
-gray16=np.int16(gray)
+gray16=np.int16(gray) #  gray = 1byte = 8bits -> 값을 잃어버리게 되어서 16bit로 늘려둠
+
+# clip연산
 emboss=np.uint8(np.clip(cv.filter2D(gray16,-1,femboss)+128,0,255))
 emboss_bad=np.uint8(cv.filter2D(gray16,-1,femboss)+128)
+
+# cv.filter(영상,-1,필터이름)
 emboss_worse=cv.filter2D(gray,-1,femboss)
 
 cv.imshow('Emboss',emboss)
