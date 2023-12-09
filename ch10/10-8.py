a5c0cd823581719d4d2377ba18a972319e972e9a
harim061 <doris5093@duksung.ac.kr>
2023-12-09 22:54:07 +0900

    10장

--- ch10/10-8.py
+++ ch10/10-8.py
@@ -24,7 +24,7 @@ while True:
         for landmarks in res.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame,landmarks,mp_hand.HAND_CONNECTIONS,mp_styles.get_default_hand_landmarks_style(),mp_styles.get_default_hand_connections_style())
 
-    cv.imshow('MediaPipe Hands',cv.flip(frame,1))	# 좌우반전
+    cv.imshow('MediaPipe Hands',cv.flip(frame,1))	# 1:좌우반전 0:상하반전 -1:좌우&상하 = 원점대칭
     if cv.waitKey(5)==ord('q'):
       break
 
